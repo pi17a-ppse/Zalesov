49be88c20340106781565af8ae90713bdd2f60d8
Илья <47445574+Zalesov99@users.noreply.github.com>
2021-02-22 17:36:05 +0300

    Инициализация доски

--- FifteenPuzzle/AI/Board.cs
+++ FifteenPuzzle/AI/Board.cs
@@ -7,6 +7,26 @@ namespace FifteenPuzzle.AI
     /// </summary>
     class Board
     {
-        
+        private int[,] board;
+
+        public static int BlocksPerLine { get; private set; }
+        public static int BlocksCount { get; private set; }
+
+        private static Board instance;
+
+        private Board() { }
+
+        public static Board Instance => instance ?? (instance = new Board());
+
+        /// <summary>
+        /// Производит инициализацию доски
+        /// </summary>
+        /// <param name="startBoard">Начальное расположение пятнашек</param>
+        public void Create(int[,] startBoard)
+        {
+            board = startBoard;
+            BlocksPerLine = board.GetLength(0);
+            BlocksCount = BlocksPerLine * BlocksPerLine;
+        }
     }
 }
