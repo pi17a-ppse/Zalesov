54267ed83e7ccb71a93687d94208420185d4a2df
Илья <47445574+Zalesov99@users.noreply.github.com>
2021-02-22 17:38:09 +0300

    Меняет значения в ячейках p1 и p2

--- FifteenPuzzle/AI/Board.cs
+++ FifteenPuzzle/AI/Board.cs
@@ -28,5 +28,27 @@ namespace FifteenPuzzle.AI
             BlocksPerLine = board.GetLength(0);
             BlocksCount = BlocksPerLine * BlocksPerLine;
         }
+
+        /// <summary>
+        /// Меняет значения в ячейках p1 и p2 местами
+        /// </summary>
+        public void Swap(Point p1, Point p2)
+        {
+            var buf = this[p1];
+            this[p1] = this[p2];
+            this[p2] = buf;
+        }
+
+        public int this[Point p]
+        {
+            get { return board[(int)p.Y, (int)p.X]; }
+            set { board[(int)p.Y, (int)p.X] = value; }
+        }
+
+        public int this[int y, int x]
+        {
+            get { return this[new Point(x, y)]; }
+            set { this[new Point(x, y)] = value; }
+        }
     }
 }
